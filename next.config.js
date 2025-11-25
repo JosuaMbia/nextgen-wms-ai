46617530e077b4a3c808bcfb5034a15d6a108f2b
JosuaMbia <josuambia75@gmail.com>
2025-11-25 16:18:17 +0100

    Add Next.js configuration with image domains and webpack

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'firebasestorage.googleapis.com',
+      'lh3.googleusercontent.com'
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig
